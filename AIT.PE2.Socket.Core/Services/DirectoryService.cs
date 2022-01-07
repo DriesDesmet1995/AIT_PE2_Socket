3390d71b204a87407869a61602e510fe79332983
Dries Desmet <61475643+DriesDesmet1995@users.noreply.github.com>
2022-01-07 18:05:06 +0100

    added DirectoryService

---
+++ AIT.PE2.Socket.Core/Services/DirectoryService.cs
@@ -0,0 +1,50 @@
+﻿using AIT.PE2.Socket.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIT.PE2.Socket.Core.Services
+{
+    class DirectoryService
+    {
+        public List<FTFolder> Folders { get; private set; }
+        public DirectoryService()
+        {
+            Folders = new List<FTFolder>();
+        }
+        public void AddFolder(FTFolder fTFolder)
+        {
+            bool found = false;
+            foreach (FTFolder fol in Folders)
+            {
+                if (fol.FolderName == fTFolder.FolderName)
+                {
+                    found = true;
+                    fol.FolderName = fTFolder.FolderName;
+                    fol.FullPath = fTFolder.FullPath;
+                    fol.FullParentPath = fTFolder.FullParentPath;
+                    fol.FilesInFolder = fTFolder.FilesInFolder;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Folders.Add(fTFolder);
+            }
+        }
+        public void RemoveFolder(string folderName)
+        {
+            foreach (FTFolder fTFolder in Folders)
+            {
+                if (fTFolder.FolderName == folderName)
+                {
+                    Folders.Remove(fTFolder);
+                }
+            }
+
+        }
+
+    }
+}
