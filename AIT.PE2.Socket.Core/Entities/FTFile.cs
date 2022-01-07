481e55d6ace631b5bb14dda5c977c7fd2f9d1e78
Dries Desmet <61475643+DriesDesmet1995@users.noreply.github.com>
2022-01-07 17:56:31 +0100

    created entities

---
+++ AIT.PE2.Socket.Core/Entities/FTFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIT.PE2.Socket.Core.Entities
+{
+    public class FTFile
+    {
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+        public byte FileSize { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ AIT.PE2.Socket.Core/Entities/FTFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIT.PE2.Socket.Core.Entities
+{
+    public class FTFolder
+    {
+        public string FolderName { get; set; }
+        public string FullPath { get; set; }
+        public string FullParentPath { get; set; }
+        public ICollection<FTFile> FilesInFolder { get; set; }
+    }
+}
